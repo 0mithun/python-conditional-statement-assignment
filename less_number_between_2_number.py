5cfd99cb45a6618f4ccaf80c602403d99865891e
Mithun Halder <mithunrptc@gmail.com>
2018-12-18 23:13:26 +0600

    Complete Less Number Between 2 Number Program

---
+++ less_number_between_2_number.py
@@ -0,0 +1,7 @@
+number1 = int(input("Please Enter First Number: "))
+number2 = int(input("Please Enter Second Number: "))
+
+if(number1 < number2):
+    print("Less Number is: {}".format(number1))
+else:
+    print("Less Number is: {}".format(number2))
