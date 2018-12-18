8f4b3065881273069b154ffa9fcdce9b2c1edc17
Mithun Halder <mithunrptc@gmail.com>
2018-12-18 23:24:43 +0600

    Complete less number between 3 number program complete

---
+++ less_number_between_3_number.py
@@ -0,0 +1,10 @@
+number1 = int(input("Pleae Enter First Number: "))
+number2 = int(input("Pleae Enter Second Number: "))
+number3 = int(input("Pleae Enter Third Number: "))
+
+if((number1 < number2) and (number1 < number3)):
+    print("Less Number is: {}".format(number1))
+elif(number2 < number3):
+    print("Less Number is: {}".format(number2))
+else:
+    print("Less Number is: {}".format(number3))
